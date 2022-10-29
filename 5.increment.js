e492908627454a83c009668d5285d4a4e447d210
anyudbwls <jyj15938@gmail.com>
2022-10-29 16:32:00 +0900

    연산

---
+++ 5.increment.js
@@ -0,0 +1,16 @@
+//증가 & 감소 연산자 Increment &Decrement
+
+let a = 0;
+a++;
+console.log(a);
+a--;
+console.log(a);
+
+// ⚠️ a++ 필요한 연산을 하고 그뒤 값 증가
+// ++a 값을 먼저 증가하고 필요한 연산을 함
+
+a = 0;
+// console.log(a++);
+// console.log(a);
+let b = a++; // 0을 b에 할당후 +연산
+console.log(b);
